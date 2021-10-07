7a4c37e69218b4dfc72c587a1c055653fae479cd
GiovanyFT <giovanyfrossard@gmail.com>
2021-10-07 19:43:06 -0300

    Suporte a vídeo no Aplicativo (plugin video_player: ^2.2.5)

--- lib/telas/tela_ajuda.dart
+++ lib/telas/tela_ajuda.dart
@@ -1,44 +1,69 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
 //import 'package:native_video_view/native_video_view.dart';
+import 'package:video_player/video_player.dart';
 
-class TelaAjuda extends StatelessWidget {
+class TelaAjuda extends StatefulWidget {
+  @override
+  _TelaAjudaState createState() => _TelaAjudaState();
+}
+
+class _TelaAjudaState extends State<TelaAjuda> {
+  late VideoPlayerController _videoPlayerController;
+  bool startedPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    String string_video;
+
+    if(Platform.isAndroid){
+      string_video = 'assets/video/apresentando_aplicativo_android.mp4';
+    } else {
+      string_video = 'assets/video/apresentando_aplicativo_ios.mp4';
+    }
+
+    _videoPlayerController =
+        VideoPlayerController.asset(string_video);
+    _videoPlayerController.addListener(() {
+      if (startedPlaying && !_videoPlayerController.value.isPlaying) {
+        Navigator.pop(context);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  Future<bool> started() async {
+    await _videoPlayerController.initialize();
+    await _videoPlayerController.play();
+    startedPlaying = true;
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Ajuda'),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        color: Colors.yellow,
-  /*      child: NativeVideoView(
-          keepAspectRatio: true,
-          showMediaController: true,
-          onCreated: (controller) {
-            if(Platform.isAndroid){
-              controller.setVideoSource(
-                'assets/video/apresentando_aplicativo_android.mp4',
-                sourceType: VideoSourceType.asset,
+    return Material(
+      elevation: 0,
+      child: Center(
+        child: FutureBuilder<bool>(
+          future: started(),
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            if (snapshot.data == true) {
+              return AspectRatio(
+                aspectRatio: _videoPlayerController.value.aspectRatio,
+                child: VideoPlayer(_videoPlayerController),
               );
             } else {
-              controller.setVideoSource(
-                'assets/video/apresentando_aplicativo_ios.mp4',
-                sourceType: VideoSourceType.asset,
-              );
+              return const Text('Aguarde o vídeo carregar');
             }
           },
-          onPrepared: (controller, info) {
-            controller.play();
-          },
-          onError: (controller, what, extra, message) {
-            print('Player Error ($what | $extra | $message)');
-          },
-          onCompletion: (controller) {
-            print('Video completed');
-          },
         ),
-      */
       ),
     );
   }
