2ac3d4b19bf7470a14d060416354d8b8086bde6d
GiovanyFT <giovanyfrossard@gmail.com>
2021-10-06 21:03:24 -0300

    Comentários sobre versões do Android

--- lib/util/toast.dart
+++ lib/util/toast.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
+// No Android 30 a customização não funcionou, mas aparece o Toast
+// Até o 29 está ok, inclusive a customização
 void MensagemAlerta(String msg){
   Fluttertoast.showToast(
       msg: msg,
