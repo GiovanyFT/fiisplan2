990aa57aeb66437098d1bb89e8930346726d221f
GiovanyFT <giovanyfrossard@gmail.com>
2021-10-08 10:45:11 -0300

    Backup feito (falta o restore)

--- lib/database_storage.dart
+++ lib/database_storage.dart
@@ -1,12 +1,21 @@
 import 'dart:io';
 
+import 'package:fiisplan2/util/toast.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 import 'controle/fabrica_contoladora.dart';
 import 'dominio/usuario.dart';
 
+/* Para ver o dispositivo no Device File Explorer é necessário:
+=> Verificar se o Google USB Driver está instalado (SDK Manager->SDK Tools)
+=> Verificar se o Android SDK está configurado no Projeto (Project Struture->
+Project SDK
+ */
 
 class DataBaseStorage {
+  static final String diretorio_imagens = "/data/data/app.fiisplan.fiisplan2/app_flutter/";
+  static final String banco = "/data/data/app.fiisplan.fiisplan2/databases/fundos.db";
+
   static Future<List<File>> filesInDirectory(Directory dir) async {
     List<File> files = <File>[];
     await for (FileSystemEntity entity
@@ -24,42 +33,51 @@ class DataBaseStorage {
     var storageRef = FirebaseStorage.instance.ref("/$nome_arquivo/");
 
     Reference storageReference = storageRef.child("$nome_arquivo.db");
-    await storageReference.putFile(File(
-        "/data/data/com.fundos.fundosimobiliarios/databases/fundos.db"));
-    String urlFile = await storageReference.getDownloadURL();
-    print("urlFile1 => $urlFile");
+    await storageReference.putFile(File(banco));
+    await storageReference.getDownloadURL();
+
 
     Directory diretorio =
-        Directory("/data/data/com.fundos.fundosimobiliarios/app_flutter/");
+        Directory(diretorio_imagens);
     List<File> arquivos = await filesInDirectory(diretorio);
     storageRef = FirebaseStorage.instance.ref("/$nome_arquivo/imagens/");
     for (int i = 0; i < arquivos.length; i++) {
-      storageReference = storageRef.child("${arquivos[i].path.substring(52)}");
+      // substring é utilizado para pegar o nome do arquivo de imagem
+      // sem o restante do path
+      storageReference = storageRef.child("${arquivos[i].path.substring(46)}");
       await storageReference.putFile(arquivos[i]);
       urlFile = await storageReference.getDownloadURL();
-      print("arquivo$i => $urlFile");
     }
+    MensagemAlerta("Backup executado com sucesso!!!");
+
   }
 
   static Future<void> buscarBDDoStorage(String nome_arquivo) async {
     var storageRef = FirebaseStorage.instance.ref("/$nome_arquivo/");
     Reference storageReference = storageRef.child("$nome_arquivo.db");
-    DownloadTask task = (await storageReference.writeToFile(
-        File("/data/data/com.fundos.fundosimobiliarios/databases/fundos.db"))) as DownloadTask;
+    TaskSnapshot task = (await storageReference.writeToFile(
+        File(banco))) ;
 
-    task.whenComplete(() {
+
+/*
+    task.
+
+    whenComplete(() {
       Future<List<Usuario>> future = FabricaControladora.obterUsuarioControl()
           .obterUsuarios();
       future.then((usuarios) async {
         storageRef = FirebaseStorage.instance.ref("/$nome_arquivo/imagens/");
         for (Usuario usuario in usuarios) {
           if (usuario.urlFoto != null) {
-            String nome_foto = "${usuario.urlFoto!.substring(55)}";
+            String nome_foto = "${usuario.urlFoto!}";
+            String local_gravacao_imagem = "$diretorio_imagens$nome_foto";
+            print("LOCAL DE GRAVAÇÃO DA IMAGEM :$local_gravacao_imagem");
             storageReference = storageRef.child(nome_foto);
-            await storageReference.writeToFile(File(usuario.urlFoto!));
+            await storageReference.writeToFile(File(local_gravacao_imagem));
           }
         };
       });
     });
+  */
   }
 }
