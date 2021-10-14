a731dfbd00b7d37da952547f265ce0ce8cad2b4d
GiovanyFT <giovanyfrossard@gmail.com>
2021-10-13 22:59:18 -0300

    Troca da biblioteca de geocoding (localizador) e suporte a mapas

--- lib/util/localizador.dart
+++ lib/util/localizador.dart
@@ -1,22 +1,12 @@
-
-//import 'package:geocoder/geocoder.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Localizador{
   static Future<LatLng?> obterLatitudeLongitudePorEndereco(String endereco) async{
-    return null;
- /*   try{
-      final query = endereco;
 
-      var addresses = await Geocoder.local.findAddressesFromQuery(query).timeout(new Duration(seconds: 3));
-      if(addresses == null) return null;
-      var first = addresses.first;
-      if (first == null) return null;
-      else return LatLng(first.coordinates.latitude, first.coordinates.longitude);
-
-    } catch(erro){
-      return null;
-    }
-    */
+    List<Location> locations = await locationFromAddress(endereco);
+    if((locations == null) || (locations.first == null)) return null;
+    else
+      return LatLng(locations.first.latitude, locations.first.longitude);
   }
 }
