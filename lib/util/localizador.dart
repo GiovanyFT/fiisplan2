ef2bf31312388fc4ca95392b44469eca781023fa
Giovany Frossard Teixeira <giovanyfrossard@gmail.com>
2021-10-26 17:17:25 -0300

    Tratando endereços não encontrados

--- lib/util/localizador.dart
+++ lib/util/localizador.dart
@@ -4,9 +4,14 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class Localizador{
   static Future<LatLng?> obterLatitudeLongitudePorEndereco(String endereco) async{
 
-    List<Location> locations = await locationFromAddress(endereco);
-    if((locations == null) || (locations.first == null)) return null;
-    else
-      return LatLng(locations.first.latitude, locations.first.longitude);
+    try{
+      List<Location> locations = await locationFromAddress(endereco);
+      if((locations == null) || (locations.first == null)) return null;
+      else
+        return LatLng(locations.first.latitude, locations.first.longitude);
+    } on NoResultFoundException {
+      print("Endereço não encontrado");
+      return null;
+    }
   }
 }
