b55d7d46bb0bf3074dd93fef99dbf8df1a7ab8e0
GiovanyFT <giovanyfrossard@gmail.com>
2022-05-16 10:45:43 -0300

    Não permitindo usuario.urlFoto == ""

--- lib/telas/controle_interacao/controle_tela_edicao_usuario.dart
+++ lib/telas/controle_interacao/controle_tela_edicao_usuario.dart
@@ -46,7 +46,7 @@ class ControleTelaEdicacaoUsuario {
       controlador_senha.text = usuario!.senha!;
       controlador_endereco.text = usuario!.endereco!;
       tipo_usuario_selecionado = usuario!.tipo;
-      if (usuario!.urlFoto != null && usuario!.urlFoto != ""){
+      if (usuario!.urlFoto != null){
         imagem = await GerenciadoraArquivo.obterImagem(usuario!.urlFoto!);
       }
     } else {
